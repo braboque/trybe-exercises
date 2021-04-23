18626f919e55045b04e8b49b49bd7f333ba11121
Sérgio A. Barbosa <sergio.alvesbarbosa@gmail.com>
2021-04-23 15:55:46 -0300

    Exercicios sobre array IndexOf

---
+++ introducao-a-javascript-e-logica-de-programacao_1/arrays2.js
@@ -0,0 +1,18 @@
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = 'Serviços';
+
+console.log(menuServices);
+
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = 'Portfólio';
+
+console.log(indexOfPortfolio);
+
+
+//Adicione o valor "Contato" no final do array menu :
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu.push('Contato');
+
+console.log(menu);
