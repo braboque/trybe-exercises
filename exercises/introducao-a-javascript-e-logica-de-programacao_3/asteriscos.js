069db8a787410a839bc8ba04137faa32b60a00d2
Sérgio A. Barbosa <sergio.alvesbarbosa@gmail.com>
2021-04-26 20:00:39 -0300

    Exercicio 02

--- exercises/introducao-a-javascript-e-logica-de-programacao_3/asteriscos.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_3/asteriscos.js
@@ -3,9 +3,12 @@ let n = 5;
 let symbol = '*';
 let inputLine = '';
 
+//atribui * e linha 5 vezes ao inputline
 for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
   inputLine = inputLine + symbol;
 };
+
+//manda imprimir 5 vezes o valor atribuído aao inputline
 for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
   console.log(inputLine);
 };
