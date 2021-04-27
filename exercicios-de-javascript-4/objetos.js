04b841adc3b14dd67e676f2fb8c83a8ae1d5f3d4
Sérgio A. Barbosa <sergio.alvesbarbosa@gmail.com>
2021-04-27 16:10:06 -0300

    Adiciona o arquivo objetos.js

---
+++ exercicios-de-javascript-4/objetos.js
@@ -0,0 +1,10 @@
+let player = {
+    name: 'Marta',
+    lastname: 'Silva',
+    age: '34',
+    medals: {golden: 2, Silver: 3},
+    bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018],
+};
+console.log( 'A jogadora', player.name + ' ' + player.lastname, 'tem', player.age, 'anos de idade.')
+
+console.log("A jogadora Marta Silva foi eleita a melhor do mundo por ", player.bestInTheWorld);
