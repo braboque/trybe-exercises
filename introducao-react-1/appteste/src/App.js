b2c545a501bc11687a0f6851cc49615da1c08c10
Sérgio A. Barbosa <sergio.alvesbarbosa@gmail.com>
2021-06-21 15:56:42 -0300

    Hello World! em react

---
+++ introducao-react-1/appteste/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+// import HelloWorld from './HelloWorld';
+import Composer from './Component';
+
+
+function App() {
+  return <Composer/>
+}
+
+export default App;
---
+++ introducao-react-1/appteste/src/Component.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react'
+
+class Composer extends Component {
+    render(){
+       return <div><h1>Sérgio Alves Barbosa</h1>
+                <p>Estudante de Desenvolvimento Web na Trybe.</p></div>
+    }
+}
+export default Composer
